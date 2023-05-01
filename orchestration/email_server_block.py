8021c3ed60900b9e203bb56556d304d92138c5cb
Ezzaldin Mamdouh <40697781+Ezzaldin97@users.noreply.github.com>
2023-05-01 13:59:42 +0000

    Update File Structure

--- orchestration/email_server_block.py
+++ orchestration/email_server_block.py
@@ -1,10 +1,12 @@
+import sys
 from prefect_email import EmailServerCredentials
+sys.path.append('./')
 from utils.config_reader import Config
 
 conf = Config()
 
 credentials = EmailServerCredentials(
     username=conf.config["EMAIL-ADDRESS-PLACEHOLDER"],
-    password=conf.config["PASSWORD-PLACEHOLDER"]
+    password=conf.config["PASSWORD-PLACEHOLDER"],
 )
-credentials.save("email-notifier")
+credentials.save("email-notifier", overwrite = True)
