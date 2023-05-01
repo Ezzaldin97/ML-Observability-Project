bdcf2bc06fbdbd7b4683705899e9e1add1cf181d
Ezzaldin Mamdouh <40697781+Ezzaldin97@users.noreply.github.com>
2023-05-01 17:43:26 +0000

    Update inference/data monitoring flow

--- orchestration/inference_data_monitor.py
+++ orchestration/inference_data_monitor.py
@@ -2,6 +2,7 @@ from flows.extract_prepare_flow import fetch_prepare_flow
 from flows.inference_flow import inference_flow
 from flows.data_monitoring_flow import data_monitoring_report_flow
 from prefect import flow
+import argparse
 
 @flow(name="InferenceAndDataDriftFlow", description="Batch Scoring and Data Drift Report",
       log_prints=True)
@@ -13,4 +14,8 @@ def parent_flow1(month:int, year:int) -> None:
     data_monitoring_report_flow(file_path, month, year)
 
 if __name__ == "__main__":
-    parent_flow1(3, 2021)
+    parser = argparse.ArgumentParser(description = "Month and Year")
+    parser.add_argument("--month", type = int, help = "month")
+    parser.add_argument("--year", type = int, help = "year")
+    args = parser.parse_args()
+    parent_flow1(args.month, args.year)
