fa21da616ae99b08a561607dddfc0f957e85a591
Ezzaldin Mamdouh <40697781+Ezzaldin97@users.noreply.github.com>
2023-05-01 14:06:50 +0000

    Update .gitignore

--- orchestration/email_server_block.py
+++ orchestration/email_server_block.py
@@ -1,5 +1,6 @@
 import sys
 from prefect_email import EmailServerCredentials
+
 sys.path.append('./')
 from utils.config_reader import Config
 
@@ -7,6 +8,6 @@ conf = Config()
 
 credentials = EmailServerCredentials(
     username=conf.config["EMAIL-ADDRESS-PLACEHOLDER"],
-    password=conf.config["PASSWORD-PLACEHOLDER"],
+    password=conf.config["PASSWORD-PLACEHOLDER"]
 )
-credentials.save("email-notifier", overwrite = True)
+credentials.save("email-notifier")
